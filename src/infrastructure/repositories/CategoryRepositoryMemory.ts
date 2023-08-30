4e3032388f6e49d179a2ab7208ae1723a708c7a9
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-08-30 18:56:01 -0300

    feat: create categoryRepository in memory database

---
+++ src/infrastructure/repositories/CategoryRepositoryMemory.ts
@@ -0,0 +1,56 @@
+import { CategoryRepository } from '../../application/repositories/CategoryRepository';
+import { Category } from '../../entities/Category';
+import { UUID } from '../../entities/UUID';
+
+export class CategoryRepositoryMemoryDatabase implements CategoryRepository {
+  data: Category[];
+
+  constructor() {
+    this.data = [];
+  }
+
+  save(category: Category): Promise<string> {
+    const categoryID = UUID.create();
+    category.ID = categoryID;
+
+    this.data.push(category);
+
+    return new Promise(resolve => {
+      resolve(categoryID);
+    });
+  }
+
+  get(categoryID: string): Promise<Category> {
+    const category = this.data.find((category) => category.ID === categoryID);
+
+    if (!category) throw new Error('Category not found');
+
+    return new Promise(resolve => {
+      resolve(category);
+    });
+  }
+
+  update(category: Category): Promise<void> {
+    const categoryIndex = this.data.findIndex((category) => category.ID === category.ID);
+
+    if (!categoryIndex) throw new Error('Category not found');
+
+    this.data[categoryIndex] = category;
+
+    return new Promise(resolve => {
+      resolve();
+    });
+  }
+
+  delete(categoryID: string): Promise<void> {
+    const categoryIndex = this.data.findIndex((category) => category.ID === categoryID);
+
+    if (!categoryIndex) throw new Error('Category not found');
+
+    this.data.splice(categoryIndex, 1);
+
+    return new Promise(resolve => {
+      resolve();
+    });
+  }
+}
