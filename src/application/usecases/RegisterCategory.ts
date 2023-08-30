c03e8b31b4100ec0dd3f323b9e17eac196e40211
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-08-30 18:56:32 -0300

    feat: create RegisterCategory usecase

---
+++ src/application/usecases/RegisterCategory.ts
@@ -0,0 +1,26 @@
+import { Category } from '../../entities/Category';
+import { CategoryRepository } from '../repositories/CategoryRepository';
+
+export class RegisterCategory {
+  constructor(readonly categoryRepository: CategoryRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const category = new Category(input.title, input.description, input.ownerID);
+
+    await this.categoryRepository.save(category);
+
+    return {
+      id: String(category.ID)
+    };
+  }
+}
+
+type Input = {
+  title: string
+  description: string
+  ownerID: string
+}
+
+type Output = {
+  id: string
+}
