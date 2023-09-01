2595eddd0639f8e1ebb75e5b99a79bda731a49a2
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 20:06:12 -0300

    feat: create AssociateCategory usecase

---
+++ src/application/usecases/product/AssociateCategory.ts
@@ -0,0 +1,44 @@
+import { UUID } from '../../../entities/UUID';
+import { CategoryRepository } from '../../repositories/CategoryRepository';
+import { ProductRepository } from '../../repositories/ProductRepository';
+
+export class AssociateCategory {
+  constructor(
+    readonly categoryRepository: CategoryRepository,
+    readonly productRepository: ProductRepository
+  ) {}
+
+  async execute(input: Input): Promise<Output> {
+    const categoryID = UUID.validate(input.categoryID, 'category');
+    const productID = UUID.validate(input.productID, 'product');
+    const category = await this.categoryRepository.getOne(categoryID);
+    const product = await this.productRepository.getOne(productID);
+
+    product.categoryID = category.ID!;
+
+    const updatedProduct = await this.productRepository.update(product);
+
+    return {
+      ID: String(updatedProduct.ID),
+      title: updatedProduct.title,
+      description: updatedProduct.description,
+      price: updatedProduct.price,
+      categoryID: String(updatedProduct.categoryID),
+      ownerID: String(updatedProduct.ownerID)
+    };
+  }
+}
+
+type Input = {
+  productID: string,
+  categoryID: string
+}
+
+type Output = {
+  ID: string,
+  title: string,
+  description: string,
+  price: number,
+  categoryID: string,
+  ownerID: string
+}
