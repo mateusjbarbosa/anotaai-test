ed5d98b2f0af4d840fc12379b275eb54c5198f9c
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 18:18:21 -0300

    refactor: change delete param for UUID in the category repository

--- src/application/repositories/CategoryRepository.ts
+++ src/application/repositories/CategoryRepository.ts
@@ -1,7 +1,8 @@
 import { Category } from '../../entities/Category';
+import { UUID } from '../../entities/UUID';
 
 export interface CategoryRepository {
 	save (category: Category): Promise<string>
 	update (category: Category): Promise<Category>;
-	delete (categoryID: string): Promise<void>;
+	delete (categoryID: UUID): Promise<void>;
 }
