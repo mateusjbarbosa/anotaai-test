67241ac1b2ac680e1378e2ce45536a4b95937e54
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 20:04:07 -0300

    feat: add getOne method in the product repository

--- src/application/repositories/ProductRepository.ts
+++ src/application/repositories/ProductRepository.ts
@@ -2,7 +2,8 @@ import { Product } from '../../entities/Product';
 import { UUID } from '../../entities/UUID';
 
 export interface ProductRepository {
-	save (category: Product): Promise<string>
-	update (category: Product): Promise<Product>;
-	delete (categoryID: UUID): Promise<void>;
+	save (product: Product): Promise<string>
+  getOne (productID: UUID): Promise<Product>
+	update (product: Product): Promise<Product>;
+	delete (productID: UUID): Promise<void>;
 }
