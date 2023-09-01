346d06ecb0097aa2e254242b90b56ea914bec4a7
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 16:23:44 -0300

    feat: create UpdateCategory usecase

---
+++ src/application/usecases/UpdateCategory.ts
@@ -0,0 +1,33 @@
+import { Category } from '../../entities/Category';
+import { CategoryRepository } from '../repositories/CategoryRepository';
+
+export class UpdateCategory {
+  constructor(readonly categoryRepository: CategoryRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const category = new Category(input.title, input.description, input.ownerID, input.ID);
+
+    await this.categoryRepository.update(category);
+
+    return {
+      ID: String(category.ID),
+      title: category.title,
+      description: category.description,
+      ownerID: String(category.ownerID)
+    };
+  }
+}
+
+type Input = {
+  ID: string
+  title: string
+  description: string
+  ownerID: string
+}
+
+type Output = {
+  ID: string,
+  title: string,
+  description: string,
+  ownerID: string
+}
