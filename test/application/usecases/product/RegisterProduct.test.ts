2954149747890e61d1bac42fb1c5ba966e725f7e
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 19:20:13 -0300

    test: create RegisterProduct usecase

---
+++ test/application/usecases/product/RegisterProduct.test.ts
@@ -0,0 +1,61 @@
+import {
+  RegisterCategory,
+  RegisterCategoryOutput
+} from '../../../../src/application/usecases/category/RegisterCategory';
+import { RegisterProduct } from '../../../../src/application/usecases/product/RegisterProduct';
+import {
+  CategoryRepositoryMemoryDatabase
+} from '../../../../src/infrastructure/repositories/CategoryRepositoryMemory';
+import {
+  ProductRepositoryMemoryDatabase
+} from '../../../../src/infrastructure/repositories/ProductRepositoryMemory';
+
+const categoryRepository = new CategoryRepositoryMemoryDatabase();
+const ownerID = 'd49b0660-1989-4a6c-b7ae-26d2d43764a4';
+let createdCategory: RegisterCategoryOutput;
+
+describe('RegisterProduct usecase', () => {
+  beforeAll(async () => {
+    const registerCategoryUsecase = new RegisterCategory(categoryRepository);
+    createdCategory = await registerCategoryUsecase.execute({
+      title: 'valid_title',
+      description: 'valid_description',
+      ownerID,
+    });
+  });
+
+  it('should register a product correctly', async () => {
+    const productRepository = new ProductRepositoryMemoryDatabase();
+    const usecase = new RegisterProduct(productRepository);
+
+    const output = await usecase.execute({
+      title: 'valid_title',
+      description: 'valid_description',
+      price: 100,
+      categoryID: createdCategory.id,
+      ownerID,
+    });
+
+    expect(output.id).toBeDefined();
+    expect(productRepository.data.length).toBe(1);
+  });
+
+  it('should thow error if product title is invalid', async () => {
+    const productRepository = new ProductRepositoryMemoryDatabase();
+    const usecase = new RegisterProduct(productRepository);
+
+    // eslint-disable-next-line max-len
+    const invalidTitle = 'Nostrud cupidatat qui aliquip duis voluptate non Lorem dolor minim duis dolor et magna quis';
+
+    await expect(usecase.execute({
+      title: invalidTitle,
+      description: 'valid_description',
+      price: 100,
+      categoryID: createdCategory.id,
+      ownerID,
+    })).rejects.toThrow(
+      new Error('The product title must have a maximum of 50 characters'),
+    );
+    expect(productRepository.data.length).toBe(0);
+  });
+});
