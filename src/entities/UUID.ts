aa017789d695aa108336f1c41350c6ce54cae3cb
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 14:50:20 -0300

    feat: add calledBy param in the UUID entity

--- src/entities/UUID.ts
+++ src/entities/UUID.ts
@@ -1,12 +1,14 @@
 import crypto from 'node:crypto';
 
 export class UUID {
-  static validate(uuid: string): string {
+  static validate(uuid: string, calledBy: string): string {
+    if (!uuid) throw new Error(`The ${calledBy} UUID is required`);
+
     const uuidRegex = RegExp(
       /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/,
     );
 
-    if (!uuidRegex.test(uuid)) throw new Error('Invalid UUID');
+    if (!uuidRegex.test(uuid)) throw new Error(`The ${calledBy} UUID is invalid`);
 
     return uuid;
   }
