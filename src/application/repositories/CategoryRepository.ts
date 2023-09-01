a099d903348a4d3f449e5d2005098e18fed8267b
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 15:34:08 -0300

    refactor: remove get method of category repository

--- src/application/repositories/CategoryRepository.ts
+++ src/application/repositories/CategoryRepository.ts
@@ -2,7 +2,6 @@ import { Category } from '../../entities/Category';
 
 export interface CategoryRepository {
 	save (category: Category): Promise<string>
-	get (categoryID: string): Promise<Category>;
 	update (category: Category): Promise<void>;
 	delete (categoryID: string): Promise<void>;
 }
