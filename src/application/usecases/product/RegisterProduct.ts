6c6037835f19ef4e6ff97e963199f99c7b1daf80
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 19:19:57 -0300

    feat: create RegisterProduct usecase

---
+++ src/application/usecases/product/RegisterProduct.ts
@@ -0,0 +1,36 @@
+import { Product } from '../../../entities/Product';
+import { ProductRepository } from '../../repositories/ProductRepository';
+
+export class RegisterProduct {
+  constructor(readonly productRepository: ProductRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const product = new Product(
+      input.title,
+      input.description,
+      input.price,
+      input.categoryID,
+      input.ownerID
+    );
+
+    await this.productRepository.save(product);
+
+    return {
+      id: String(product.ID)
+    };
+  }
+}
+
+type Input = {
+  title: string
+  description: string
+  price: number
+  categoryID: string
+  ownerID: string
+}
+
+type Output = {
+  id: string
+}
+
+export { Output as RegisterProductOutput };
