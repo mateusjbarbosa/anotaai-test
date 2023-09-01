81a3fbad121cfd19b21b2e516a4076dc3f2d2134
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 18:20:00 -0300

    feat: create DeleteCategory usecase

---
+++ src/application/usecases/DeleteCategory.ts
@@ -0,0 +1,18 @@
+import { UUID } from '../../entities/UUID';
+import { CategoryRepository } from '../repositories/CategoryRepository';
+
+export class DeleteCategory {
+  constructor(readonly categoryRepository: CategoryRepository) {}
+
+  async execute(input: Input): Promise<void> {
+    const { ID } = input;
+
+    const categoryID = UUID.validate(ID, 'category');
+
+    await this.categoryRepository.delete(categoryID);
+  }
+}
+
+type Input = {
+  ID: string
+}
