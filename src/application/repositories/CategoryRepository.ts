48c06a122eb2ad16b6163acd8e43a7b3c7a9f761
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-01 20:03:49 -0300

    feat: add getOne method in the category repository

--- src/application/repositories/CategoryRepository.ts
+++ src/application/repositories/CategoryRepository.ts
@@ -2,7 +2,8 @@ import { Category } from '../../entities/Category';
 import { UUID } from '../../entities/UUID';
 
 export interface CategoryRepository {
-	save (category: Category): Promise<string>
+	save (category: Category): Promise<string>;
+  getOne (categoryID: UUID): Promise<Category>;
 	update (category: Category): Promise<Category>;
 	delete (categoryID: UUID): Promise<void>;
 }
