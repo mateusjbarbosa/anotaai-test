85a9d0da2d130167ba741236781e77295b78433f
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-02 15:11:57 -0300

    feat: (WIP) update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import express from 'express';
 import cron from 'node-cron';
+import { UpdateCatalog, UpdateCatalogAction } from './application/usecases/owner/UpdateCatalog';
+import { Product } from './entities/Product';
+import { S3Adapter } from './infrastructure/bucket/S3Adapter';
+import { MongoClientAdapter } from './infrastructure/database/MongoClientAdapter';
 import { router } from './infrastructure/http/routes';
 import { SQSAdapter } from './infrastructure/queue/SQSAdapter';
+import {
+  CategoryRepositoryMongoDatabase
+} from './infrastructure/repositories/mongodb/CategoryMongoDatabase';
+import {
+  ProductRepositoryMongoDatabase
+} from './infrastructure/repositories/mongodb/ProductMongoDatabase';
 
 const main = () => {
   try {
@@ -12,12 +22,27 @@ const main = () => {
 
     const queue = new SQSAdapter();
     cron.schedule('*/5 * * * * *', async () => {
-      await queue.receiveMessage('catalog-emit', (message: string) => {
-        const parsedMessage: {action: string, message: string} = JSON.parse(message);
+      await queue.receiveMessage('catalog-emit', async (message: string) => {
+        const parsedMessage: { action: string , message: string } = JSON.parse(message);
         const product = JSON.parse(parsedMessage.message);
+        const bucket = new S3Adapter();
+        const connection = new MongoClientAdapter().database;
+        const categoryRepository = new CategoryRepositoryMongoDatabase(connection!);
+        const productRepository = new ProductRepositoryMongoDatabase(connection!);
 
-        // eslint-disable-next-line no-console
-        console.log(product);
+        const usecase = new UpdateCatalog(bucket, categoryRepository, productRepository);
+
+        await usecase.execute({
+          action: parsedMessage.action as UpdateCatalogAction,
+          product: new Product(
+            product.title,
+            product.description,
+            product.price,
+            String(product.categoryID),
+            String(product.ownerID),
+            String(product._ID)
+          )
+        });
       });
     });
 
