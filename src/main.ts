99669633ba79e4cfb2f595f487d2135f0a0bc7d8
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-02 15:23:37 -0300

    feat: improve main.ts organization

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
 import express from 'express';
-import cron from 'node-cron';
-import { UpdateCatalog, UpdateCatalogAction } from './application/usecases/owner/UpdateCatalog';
-import { Product } from './entities/Product';
-import { S3Adapter } from './infrastructure/bucket/S3Adapter';
-import { MongoClientAdapter } from './infrastructure/database/MongoClientAdapter';
 import { router } from './infrastructure/http/routes';
-import { SQSAdapter } from './infrastructure/queue/SQSAdapter';
-import {
-  CategoryRepositoryMongoDatabase
-} from './infrastructure/repositories/mongodb/CategoryMongoDatabase';
-import {
-  ProductRepositoryMongoDatabase
-} from './infrastructure/repositories/mongodb/ProductMongoDatabase';
+import { watchCatalogUpdates } from './receive-messages';
 
 const main = () => {
   try {
@@ -20,31 +9,7 @@ const main = () => {
     app.use(express.json());
     app.use(router);
 
-    const queue = new SQSAdapter();
-    cron.schedule('*/5 * * * * *', async () => {
-      await queue.receiveMessage('catalog-emit', async (message: string) => {
-        const parsedMessage: { action: string , message: string } = JSON.parse(message);
-        const product = JSON.parse(parsedMessage.message);
-        const bucket = new S3Adapter();
-        const connection = new MongoClientAdapter().database;
-        const categoryRepository = new CategoryRepositoryMongoDatabase(connection!);
-        const productRepository = new ProductRepositoryMongoDatabase(connection!);
-
-        const usecase = new UpdateCatalog(bucket, categoryRepository, productRepository);
-
-        await usecase.execute({
-          action: parsedMessage.action as UpdateCatalogAction,
-          product: new Product(
-            product.title,
-            product.description,
-            product.price,
-            String(product.categoryID),
-            String(product.ownerID),
-            String(product._ID)
-          )
-        });
-      });
-    });
+    watchCatalogUpdates();
 
     app.listen(3000, () => {
       // eslint-disable-next-line no-console
