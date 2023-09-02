8102dcb9ae1cec6c9bf0ff6e5e39086d4a689edd
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-02 15:07:00 -0300

    feat: create S3 adapter

---
+++ src/infrastructure/bucket/S3Adapter.ts
@@ -0,0 +1,63 @@
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import AWS from 'aws-sdk';
+import { config } from 'dotenv';
+import { Bucket } from './Bucket';
+
+export class S3Adapter implements Bucket {
+  private s3: AWS.S3;
+  private s3Client: S3Client;
+
+  constructor() {
+    config();
+
+    const s3Params = {
+      apiVersion: 'latest',
+      region: 'us-east-1',
+      credentials: new AWS.SharedIniFileCredentials({ profile: 'default' })
+    };
+
+    this.s3 = new AWS.S3(s3Params);
+    this.s3Client = new S3Client(s3Params);
+  }
+
+  async createObject(bucketName: string, objectName: string, data: string): Promise<void> {
+    await this.s3.upload({
+      Bucket: bucketName,
+      Key: objectName,
+      Body: data
+    }).promise();
+  }
+
+  async listObjects(bucketName: string): Promise<string[]> {
+    const result = await this.s3.listObjects({
+      Bucket: bucketName
+    }).promise();
+
+    const objects: string[] = [];
+    if (result.Contents) {
+      result.Contents.forEach((object) => objects.push(object.Key || ''));
+    }
+
+    return objects;
+  }
+
+  async getObject(bucketName: string, objectKey: string): Promise<string> {
+    try {
+      const fileRequestCommand = new GetObjectCommand({
+        Bucket: bucketName,
+        Key: objectKey
+      });
+
+      const request = await this.s3Client.send(fileRequestCommand);
+
+      const response = await request.Body?.transformToString();
+
+      return response || '';
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      // eslint-disable-next-line no-console
+      console.log(error.message);
+      throw new Error(`Error during get object of bucket ${bucketName} with key ${objectKey}`);
+    }
+  }
+}
