61c44df69db551c3c8f49d17ea958194e1389ccd
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-02 00:32:41 -0300

    feat: add queue interface in the SQS adapter and update config

--- src/infrastructure/queue/SQSAdapter.ts
+++ src/infrastructure/queue/SQSAdapter.ts
@@ -1,26 +1,41 @@
 import AWS from 'aws-sdk';
 import { config } from 'dotenv';
+import { Queue } from './Queue';
 
-export class SQSAdapter {
+export class SQSAdapter implements Queue {
   private service: AWS.SQS;
 
   constructor() {
     config();
 
     this.service = new AWS.SQS({
-      apiVersion: 'latest'
+      apiVersion: 'latest',
+      region: 'us-east-1',
+      credentials: new AWS.SharedIniFileCredentials({ profile: 'default' })
     });
   }
 
-  async sendMessage(queue: string, message: string): Promise<void> {
+  async sendMessage(queue: string, message: string, action: string): Promise<void> {
     try {
       await this.service.sendMessage({
         QueueUrl: `https://sqs.us-east-1.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/${queue}`,
-        MessageBody: message
+        MessageBody: message,
+        MessageAttributes: {
+          'Action': {
+            DataType: 'String',
+            StringValue: action
+          }
+        }
       }).promise();
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
+      // eslint-disable-next-line no-console
+      console.log(error.message);
       throw new Error('Error during send the message');
     }
+  }
+
+  receiveMessage(): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
